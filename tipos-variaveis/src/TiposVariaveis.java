33e51e0e2a5cac6355fdad68a0fca97e5ea57931
Jorge Filho <jorge.munizfilho@gmail.com>
2024-11-23 15:02:50 -0300

    Tipos numericos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,6 +2,15 @@
 public class TiposVariaveis {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+
+        int numeroNormal = numeroCurto;
+
+        short numeroCurto2 = (short) numeroNormal;
+
     }
+
 }
